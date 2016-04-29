cec71079ea528504de52f7f013052291f9573c0c
aloukili <tech.abdel@gmail.com>
2016-04-29 16:02:18 +0200

    add function finlot to request in big number id istex

--- index.js
+++ index.js
@@ -43,3 +43,42 @@ exports.find = function (search,  callback) {
 	});
 };
 
+exports.findlot = function (search, callback) {
+
+    var urlistex = 'https://api.istex.fr/document/?size=200&output=*&q=id:(';
+	if (search && search.length != 0)  {
+		urlistex += search.shift();
+	} else {
+		return callback(new Error("index istex is incorrect"));
+	}
+	for (var i = 0; i < search.length; i++){
+		var urlistex = urlistex + ' OR ' + search[i] ;
+	}
+
+	var urlistex = urlistex + ')';
+
+	var options = {
+	  url: urlistex,
+	  headers: {
+	    'User-Agent': 'ezpaarse'
+	  }
+	};
+
+	request.get(options, function (err , req , body){
+		if (err) { return callback(err); }
+		
+			
+		try {
+			var result = JSON.parse(body);
+		} catch (e) {
+			return callback(e);
+		}
+
+		if (result.total == 0 && result.error) {
+			return callback(new Error('erreur de requette'));
+		}
+		callback(null, result.hits[0]);
+	
+	});
+};
+
