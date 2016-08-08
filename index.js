16944c61fb6cad55893aa513cee839fb6ef0c971
aloukili <tech.abdel@gmail.com>
2016-08-08 11:45:53 +0200

    refactoring modul istex

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 
-var request = require('request').defaults({
-	proxy: process.env.http_proxy ||
-	process.env.HTTP_PROXY ||
-	process.env.https_proxy ||
-	process.env.HTTPS_PROXY
-	});
+var request = require('request').defaults({proxy: process.env.http_proxy ||
+process.env.HTTP_PROXY ||
+process.env.https_proxy ||
+process.env.HTTPS_PROXY
+});
 
 /**
 * @param {Object} search
@@ -14,71 +13,68 @@ var request = require('request').defaults({
 */
 exports.find = function (search,  callback) {
 
-	if (!search && search.length != 40)  {
-		return callback(new Error("index istex is incorrect"));
-	}
-	var urlistex = 'https://api.istex.fr/document/' + search ;
-	var options = {
-	  url: urlistex,
-	  headers: {
-	    'User-Agent': 'ezpaarse'
-	  }
-	};
+  if (!search && search.length != 40) {
+    return callback(new Error('index istex is incorrect'));
+  }
+  var urlistex = 'https://api.istex.fr/document/' + search ;
+  var options = {
+    url: urlistex,
+    headers: {
+      'User-Agent': 'ezpaarse'
+    }
+  };
 
-	request.get(options, function (err , req , body){
-		if (err) { return callback(err); }
-		
-			
-		try {
-			var result = JSON.parse(body);
-		} catch (e) {
-			return callback(e);
-		}
+  request.get(options, function (err, req, body) {
+    if (err) { return callback(err); }
 
-		if (result.error) {
-			return callback(new Error(result.error));
-		}
-		 callback(null, result);
-	
-	});
+    try {
+      var result = JSON.parse(body);
+    } catch (e) {
+      return callback(e);
+    }
+
+    if (result.error) {
+      return callback(new Error(result.error));
+    }
+    callback(null, result);
+
+  });
 };
 
 exports.findlot = function (search, callback) {
 
-    var urlistex = 'https://api.istex.fr/document/?size=200&output=*&q=id:(';
-	if (search && search.length != 0)  {
-		urlistex += search.shift();
-	} else {
-		return callback(new Error("index istex is incorrect"));
-	}
-	for (var i = 0; i < search.length; i++){
-		var urlistex = urlistex + ' OR ' + search[i] ;
-	}
+  var urlistex = 'https://api.istex.fr/document/?size=200&output=*&q=id:(';
+  if (search && search.length != 0)  {
+    urlistex += search.shift();
+  } else {
+    return callback(new Error('index istex is incorrect'));
+  }
+  for (var i = 0; i < search.length; i++) {
+    urlistex = urlistex + ' OR ' + search[i] ;
+  }
 
-	var urlistex = urlistex + ')';
+  urlistex = urlistex + ')';
 
-	var options = {
-	  url: urlistex,
-	  headers: {
-	    'User-Agent': 'ezpaarse'
-	  }
-	};
+  var options = {
+    url: urlistex,
+    headers: {
+      'User-Agent': 'ezpaarse'
+    }
+  };
+  request.get(options, function (err, req, body) {
+    if (err) { return callback(err); }
 
-	request.get(options, function (err , req , body){
-		if (err) { return callback(err); }
-		
-			
-		try {
-			var result = JSON.parse(body);
-		} catch (e) {
-			return callback(e);
-		}
+    try {
+      var result = JSON.parse(body);
+    } catch (e) {
+      return callback(e);
+    }
 
-		if (result.total == 0 && result.error) {
-			return callback(new Error('erreur de requette'));
-		}
-		callback(null, result.hits);
-	
-	});
+    if (result.total == 0 && result.error) {
+      return callback(new Error('erreur de requette'));
+    }
+    callback(null, result.hits);
+
+  });
 };
 
